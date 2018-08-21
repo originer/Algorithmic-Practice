8e5ce7ef9b72a2d0f877d7efe42d7bfd587172f8
horizon <997072815@qq.com>
2018-08-21 20:31:08 +0800

    leetcode:problem07

---
+++ src/main/java/com/shekhargulati/leetcode/algorithms/Problem07.java
@@ -0,0 +1,38 @@
+package com.shekhargulati.leetcode.algorithms;
+
+/**
+ * 给定一个 32 位有符号整数，将整数中的数字进行反转
+ *  示例 1:
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ * 输入: -123
+ * 输出: -321
+ *  示例 3:
+ * 输入: 120
+ * 输出: 21
+ *
+ * Created by Horizon on 2018/8/21.
+ */
+public class Problem07 {
+
+    /**
+     * 值得注意的是 120 -> 021
+     * Integer.valueOf()的方法可以帮助去除前面的0
+     * 使用 try catch 捕获翻转后超出int范围的值
+     *
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        try {
+            if (x >= 0) {
+                return Integer.valueOf(new StringBuilder(String.valueOf(x)).reverse().toString());
+            } else {
+                return -Integer.valueOf(new StringBuilder(String.valueOf(-x)).reverse().toString());
+            }
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/shekhargulati/leetcode/algorithms/Problem07Test.java
@@ -0,0 +1,27 @@
+package com.shekhargulati.leetcode.algorithms;
+
+import org.junit.Test;
+
+/**
+ * Created by Horizon on 2018/8/21.
+ */
+public class Problem07Test {
+    @Test
+    public void reverse() {
+        Problem07 problem07 = new Problem07();
+        int num = 123;
+        System.out.println(problem07.reverse(num));
+        int num1 = -123;
+        System.out.println(problem07.reverse(num1));
+        int num2 = 1534236469;
+        System.out.println(problem07.reverse(num2));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.valueOf(String.valueOf(new StringBuilder("1534236469").reverse())));
+
+        Integer in = new Integer(12);
+        System.out.println(-in);
+    }
+
+}
