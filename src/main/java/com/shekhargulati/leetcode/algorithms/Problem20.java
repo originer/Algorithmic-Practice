2bd118db98fc5e6ba30a9ad0ebc4a7f479e97fec
horizon <997072815@qq.com>
2018-08-29 09:20:06 +0800

    leetcode:problem20

---
+++ src/main/java/com/shekhargulati/leetcode/algorithms/Problem20.java
@@ -0,0 +1,36 @@
+package com.shekhargulati.leetcode.algorithms;
+
+import java.util.Stack;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ */
+public class Problem20 {
+
+    /**
+     * 利用栈来解决问题，最后一个stack.pop是精髓，思路牛逼
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
+}
