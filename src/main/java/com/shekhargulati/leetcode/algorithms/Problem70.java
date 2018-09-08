297e82da8f29706a77d9a0e5fd5357ed1c4d607f
horizon <997072815@qq.com>
2018-09-08 16:22:27 +0800

    leetcode:problem21 、problem70

---
+++ src/main/java/com/shekhargulati/leetcode/algorithms/Problem70.java
@@ -0,0 +1,37 @@
+package com.shekhargulati.leetcode.algorithms;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 数值有规律
+ * Created by Horizon on 2018/9/8.
+ */
+public class Problem70 {
+
+    /*递归*/
+    //public static int climbStairs(int n) {
+    //    int re = 0;
+    //    if (n == 1 || n == 0) {
+    //        return 1;
+    //    } else {
+    //        re = climbStairs(n - 1) + climbStairs(n - 2);
+    //    }
+    //    return re;
+    //}
+
+    /*非递归*/
+    public static int climbStairs(int n) {
+        int re = 0, a = 1, b = 1;
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+        while (n > 1) {
+            re = a + b;
+            a = b;
+            b = re;
+            n--;
+        }
+        return re;
+    }
+}
