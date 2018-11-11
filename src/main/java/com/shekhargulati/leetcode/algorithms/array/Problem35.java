1d7598ed029ff413b93406b2e26d8a969a59e990
horizon <997072815@qq.com>
2018-11-11 22:32:50 +0800

    add new

--- src/main/java/com/shekhargulati/leetcode/algorithms/array/Problem35.java
+++ src/main/java/com/shekhargulati/leetcode/algorithms/array/Problem35.java
@@ -7,5 +7,21 @@ package com.shekhargulati.leetcode.algorithms.array;
  * Created by Horizon on 2018/11/11.
  */
 public class Problem35 {
+    public static int searchInsert(int[] nums, int target) {
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            } else if (nums[i] > target) {
+                return i - 1 < 0 ? 0 : i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, 5, 6};
+
+        System.out.println(searchInsert(nums, 7));
+    }
 }
